5d93fbe4fd388304b2a7d3cfa95024fb398a8189
rengimcetingoz <50210194+rengimcetingoz@users.noreply.github.com>
2025-04-07 01:31:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,1 @@
-from setuptools import setup
 
-setup(
-   name='gan_portfolios',
-   version='1.1',
-   description='...',
-   author='CAL',
-   author_email='CAL',
-   packages=['gan_portfolios'],  #same as name
-   install_requires=[], #external packages as dependencies
-)
